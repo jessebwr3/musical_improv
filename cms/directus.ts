61490a93b5f48df72832c09c8b67708ad1cfb3ca
Jesse Watts-Russell <jessebwr3@gmail.com>
2022-11-06 23:25:14 -0500

    moving to directus CMS

---
+++ cms/directus.ts
@@ -0,0 +1,45 @@
+import { Directus, OneItem, QueryOne } from '@directus/sdk';
+
+// Map your collection structure based on its fields.
+type Teammate = {
+  id: string;
+  sort: number;
+  my_story: string;
+  primary_image: string;
+  photos?: [string];
+};
+
+// Map your collections to its respective types. The SDK will
+// infer its types based on usage later.
+type App = {
+  // [collection_name]: typescript_type
+  teammates: Teammate;
+};
+
+export type OneItemTeammate = OneItem<Teammate, QueryOne<Teammate>, false>;
+export type Teammates = OneItemTeammate[] | null | undefined;
+
+const rootUrl = 'https://3fjxwboo.directus.app';
+
+const directus = new Directus<App>(rootUrl);
+
+const teammatesCollection = directus.items('teammates');
+
+export const getTeammates = async (): Promise<Teammates> => {
+  const teammatesQuery = await teammatesCollection.readByQuery({ limit: -1 });
+
+  return teammatesQuery.data;
+};
+
+export const getTeammateById = async (id?: string): Promise<OneItemTeammate> => {
+  if (!id) {
+    return null;
+  }
+
+  const teammate = await teammatesCollection.readOne(id);
+  return teammate;
+};
+
+export const getAssetUrl = (id?: string) => `${rootUrl}/assets/${id}`;
+
+export default directus;
